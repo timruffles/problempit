b5597cd51be14623db8646a67df058651400a350
Tim Ruffles <timruffles@Tim-Ruffless-MacBook-Pro.local>
2010-12-26 23:06:01 +0000

    Realised there's a better way to write my matchers

--- spec/acceptance/problem/create_a_problem.spec
+++ spec/acceptance/problem/create_a_problem.spec
@@ -2,6 +2,14 @@ feature "So that I can log a problem on the site
 As a user
 I want a problem entry screen
 " do
-  it { should_create_with_valid_attributes }
-  
+  it {
+    visit('problem/new')
+    submit_validly_filled_new_form_for(Problem)
+    should have_created_an_instance_with_message
+  }
+  it {
+    visit('problem/new')
+    submit_invalidly_filled_new_form_for(Problem)
+    should_not have_created_an_instance_with_message
+  }
 end
--- spec/helpers/acceptance/crud_features.rb
+++ spec/helpers/acceptance/crud_features.rb
@@ -11,80 +11,77 @@ class LazyAccept
   def state
     @state ||= State.new
   end
-  module HelperClassMethods
-    def wrap_with_message(to_wrap, default)
-      # setup a message expectation with a default, and call original
-      define_method "#{method}_with_message" do |*args|
-        args_req = method(to_wrap)
-        message = if args.length > args_req
-                    args.pop
-                  else
-                    default
-                  end
-        page.should have_content(message)
-        send(to_wrap)
-      end
-    end
-  end
-  module ModelHelpers
-    extend HelperClassMethods
+  module WebHelpers
     def model_params
       params[LazyAccept.state.testing_model.to_s.tableize]
     end
     ['new','edit'].each do |action|
-      define_method "submit_validly_filled_#{action}_form_for", do |model|
-        fill_and_submit("#new_#{model.tableize}", Factory.attributes_for(model.tableize})
+      define_method "submit_validly_filled_#{action}_form_for" do |model|
+        fill_and_submit("#new_#{model.tableize}", Factory.attributes_for(model.tableize))
         LazyAccept.state.testing_model(model)
       end
-      define_method "submit_invalidly_filled_#{action}_form_for", do |model|
+      define_method "submit_invalidly_filled_#{action}_form_for" do |model|
         fill_and_submit("#new_#{model.tableize}", Factory.attributes_for("invalid_#{model.tableize}"))
         LazyAccept.state.testing_model(model)
+      end
+      def fill_and_submit(form, attributes)
+        attributes.each do |attribute, val|
+          within(form) { fill_in attribute.to_s, val}
+        end
+        within(form) { click_button '#submit' }
+      end
+    end
+  end
+  # define all our matchers
+  module MatcherDefinitions
+    def wrap_with_message(to_wrap, definitions, default)
+      # setup a message expectation with a default, and call original
+      ['','not_'].each do |neg|
+        definitions["#{neg}#{method}_with_message".to_sym] = lambda do |*args|
+          args_req = method(to_wrap)
+          message = if args.length > args_req
+                      args.pop
+                    else
+                      default
+                    end
+          page.send(neg == '' ? :should : :should_not, have_content(message))
+          should(to_wrap)
+          true # original matcher passes
+        end
+      end
+    end
+    DEFINITIONS = {
+      :have_created_an_instance => lambda do
+        # if the newest model equals sent attribs, we've created
+        LazyAccept.state.testing_model.tap do |model|
+          #model.count.should == s.current_count + 1
+          newest_attribs = model.first.attributes
+          model_params.all? do |k,v|
+            # TODO work for defaults
+            newest_attribs[k].should == v if v 
+          end
+        end
+      end,
+      :have_updated_an_instance => lambda do
+        # if the updated model has equal attribs, we've updated
+        LazyAccept.state.testing_model.tap do |model|
+          # TODO work for defaults
+          model.find(model_params[:id]).attributes.all? do |k,v|
+            params[k] == v if model_params[k]
+          end
+        end
+      end
+    }
+    wrap_with_message :have_created_an_instance, DEFINTIIONS
+    wrap_with_message :have_updated_an_instance, DEFINTIIONS
+    
+    include Spec::Matchers
+    DEFINITIONS.each do |matcher, implementation|
+      Spec::Matchers.define matcher do
+        match(&implementation)
       end
     end
   end
   
-  # define all our matchers
-  module MatcherDefinitions
-    pre = methods.dup
-    def have_created_an_instance(msg)
-      LazyAccept.state.testing_model.tap do |model|
-        #model.count.should == s.current_count + 1
-        newest_attribs = model.first.attributes
-        model_params.each do |k,v|
-          # TODO work for defaults
-          newest_attribs[k].should == v if v 
-        end
-      end
-    end
-    wrap_with_message :should_have_created_an_instance
-    def have_updated_an_instance(msg)
-      LazyAccept.state.testing_model.tap do |model|
-        model.find(model_params[:id]).attributes.each do |k,v|
-          params[k].should == v if model_params[k]
-        end
-      end
-    end
-    wrap_with_message :should_have_updated_an_instance
-    MATCHERS_DEFINED = (methods - pre)
-  end
-  module Matchers
-    extend MatcherDefinitions
-    MatcherDefinitions::MATCHERS_DEFINED.each do |matcher|
-      Spec::Matchers.define matcher.to_sym do
-        match do
-          
-        end
-      end
-    end
-  end
-  module WebHelpers
-    extend HelperClassMethods
-    def fill_and_submit(form, attributes)
-      attributes.each do |attribute, val|
-        within(form) { fill_in attribute.to_s, val}
-      end
-      within(form) { click_button '#submit' }
-    end
-  end
 end
 
