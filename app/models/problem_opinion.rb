f8e8b59fd281febda92fb6a8324ee2e0d4fc50b2
Tim Ruffles <timruffles@Tim-Ruffless-MacBook-Pro.local>
2011-01-03 18:13:06 +0000

    Defined ProblemOpinion options

--- app/models/problem_opinion.rb
+++ app/models/problem_opinion.rb
@@ -1,2 +1,8 @@
 class ProblemOpinion
+  ordinal_field :who_size,  ['Micro','Niche','Mainstream']
+  ordinal_field :pain,      ['Novelty','Obstacle','Vital']
+  ordinal_field :awareness, ['None','Aware','Improvising','Procuring']
+  ordinal_field :solutions, ['None','Unsettled','Mature']
+  ordinal_field :growth,    ['Decline','Static','Growth']
 end
+
